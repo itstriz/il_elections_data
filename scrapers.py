e66cbd9c1782459daa4aaf358e22d2a012f47165
Matt Johnson <mjohn23@gmail.com>
2015-05-08 14:53:29 -0500

    Minor cleanup

--- scrapers.py
+++ scrapers.py
@@ -3,6 +3,22 @@ import urllib2
 
 def scrape_a1(url):
     soup = url_to_soup(url)
+    if not soup:
+        return False
+
+    # Fields to scrape:
+    # -----------------
+    # Contributed By    Name in format (Last, First Middle
+    #   Occupation:     (optional) Job in format 'Occupation: <Job>'
+    #   Employer:       (optional) Employer in format 'Employer: <Employer>'
+    # Address
+    # Amount:           $#,###.##
+    #   Date            m/d/yyyy
+    # Report Type       ##
+    #   Received by     Hyperlinked text with link to Committee page
+    # Description
+    # Vendor Name
+    # Vendor Address
 
     committee_name = soup.find('span', {'id': 'ctl00_ContentPlaceHolder1_lblName'}).text
     data_table = soup.find('table', {'id': 'ctl00_ContentPlaceHolder1_tblA1List'})
@@ -28,14 +44,18 @@ def scrape_a1(url):
                 'receipt_type': receipt_type,
         }
 
-        print data
+        return data
 
 def url_to_soup(url):
-    response = urllib2.urlopen(url)
+    """ Takes an url and returns a BS4 object of the page at that url. Assumes a valid url """
+    try:
+        response = urllib2.urlopen(url)
+    except:
+        return False
     html = response.read()
     return BeautifulSoup(html)
 
 
-a1_url = 'http://www.elections.il.gov/CampaignDisclosure/A1List.aspx?ID=22989&FiledDocID=567064&ContributionType=AllTypes&Archived=False'
+a1_url = 'http://www.elections.il.gov/CampaignDisclosure/A1List.aspx?ID=577607&FiledDocID=577607&ContributionType=All%20Types&Archived=False'
 
-scrape_a1(a1_url)
+print scrape_a1(a1_url)
